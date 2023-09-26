14456448bfcb6fc11a6da97c77b8b22c8b3aca9e
Pasquale <rmmpql@gmail.com>
2023-09-26 23:44:20 +0200

    Update logout_db.php
    generalize host url  with:
    - $url="https://". $_SERVER['HTTP_HOST'] ."/index.php";

--- webApp/code/php/Login/logout_db.php
+++ webApp/code/php/Login/logout_db.php
@@ -1,13 +1,12 @@
 <?php
 // Start the session
 session_start();
-$url="https://myfishtank.altervista.org/index.php";
+$url="https://". $_SERVER['HTTP_HOST'] ."/index.php";
 // Destroy the session.
 if(session_destroy()){
   session_commit();
   header("Location: ".$url);
   exit('success');
-  
 }else{  
   exit('failed');
 }
